519889ea31d22bc63516d85e6646ade765fea65d
HadiOgut198 <ogutogut19@gmail.com>
2023-04-13 02:37:39 +0700

    First commit

---
+++ index-pertama.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, sunt possimus asperiores laborum ipsa temporibus itaque laboriosam corrupti perspiciatis dolores tempore voluptate quo exercitationem. Omnis molestias voluptatum explicabo earum autem.
+    Sunt numquam et autem fugit ut architecto repellendus! Vero sit dicta cupiditate. Consequuntur, placeat reprehenderit dicta odit quidem voluptates delectus doloremque tempora necessitatibus alias magnam ab quos, quas inventore in!
+    Veniam laudantium, reiciendis similique sed qui reprehenderit iusto? Facere tempore incidunt eligendi consectetur. Quisquam laboriosam delectus dolor, debitis assumenda voluptas tempore est necessitatibus, accusantium nisi veritatis corporis, voluptatibus quasi rem?
+</body>
+
+</html>
